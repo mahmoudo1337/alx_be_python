289d8e7ed59dcb2775a62e5bcc19a73d8f00bf57
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-06-18 02:41:21 +0300

    first exercise

---
+++ python/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition = int(number1) + int(number2)
+subtraction = int(number1) - int(number2)
+multiplication = int(number1) * int(number2)
+
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(addition)) 
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(subtraction)) 
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(multiplication)) 
