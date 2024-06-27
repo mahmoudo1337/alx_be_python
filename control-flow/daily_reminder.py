61aac3c9d81941bbc595eadbf856371cf32f7b53
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-06-27 03:36:45 +0300

    daily reminder2

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,23 +1,25 @@
 task = input("Enter your task:")
 priority = input("Priority (high/medium/low):")
 time_bound = input("Is it time-bound? (yes/no):")
+reminder = f"Reminder: '{task} is a {priority} priority task that requires immediate attention today!'"
+note = f"Note: '{task} is a {priority}. Consider completing it when you have free time.'"
 
 match priority:
     case "high":
         if time_bound == "yes":  
-            print(f"Reminder: '{task} is a {priority} priority task that requires immediate attention today!'")
+            print(reminder)
         elif time_bound == "no":
-            print(f"Note: '{task} is a {priority}. Consider completing it when you have free time.'")
+            print(note)
     case "medium":
         if time_bound == "yes":  
-            print(f"Reminder: '{task} is a {priority} priority task that requires immediate attention today!'")
+            print(reminder)
         elif time_bound == "no":
-            print(f"Note: '{task} is a {priority}. Consider completing it when you have free time.'")
+            print(note)
     case "low":
         if time_bound == "no":
-            print(f"Note: '{task} is a {priority} priority task. Consider completing it when you have free time.'")
+            print(note)
         elif time_bound == "yes":
-            print(f"Reminder: '{task} is a {priority} priority task that requires immediate attention today!'")
+            print(reminder)
     case _:
         print("Invalid Input")
         
