3410f9e3b90eb82948297bd1644016afc8114948
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-07-20 03:26:40 +0300

    v3

---
+++ oop/library_system.py
@@ -0,0 +1,25 @@
+class Book:
+    def __init__(self, title, author):
+      self.title = title
+      self.author = author
+      
+      
+      
+class EBook(Book):
+    def __init__(self, file_size):
+      self.file_size = file_size
+
+class PrintBook(Book):
+    def __init__(self, name, page_count):
+      self.page_count = page_count
+
+
+class Library(Book):
+    def __init__(self, books):
+      self.books = []
+      
+    def add_book(self, book):
+        self.books.append(book)
+    def list_books(self):
+        for book in self.books:
+            print(book)
