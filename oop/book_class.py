31f9e903e3c68bd7064486f273e0de6c7443dac7
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-07-20 03:20:44 +0300

    v3

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,8 +1,8 @@
 class Book:
     def __init__(self, title, author, year):
-      self.title = title
-      self.author = author
-      self.year = year
+      self.title = str(title)
+      self.author = str(author)
+      self.year = int(year)
     def __str__(self):
         return f"{self.title} by {self.author}, published in year {self.year}"
     def __del__(self):
